0dcf36579664558d06ee6678a97e1a20ec65b19e
alok7139 <alokgarg7139@gmail.com>
2024-12-19 12:43:38 +0530

    commit

--- client/src/Components/Bgslider.jsx
+++ client/src/Components/Bgslider.jsx
@@ -9,7 +9,7 @@ function Bgslider() {
         setsliderposition(e.target.value);
     }
   return (
-    <div>
+    <div className='pb-10 md:py-20 mx-2'>
          {/* title */}
         <h1 className='mb-12 sm:mb-20 text-center text-2xl md:text-3xl lg:text-4xl 
         mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 
