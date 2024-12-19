ef078599d2f44dfb400afb85287fdb1ec1d67147
alok7139 <alokgarg7139@gmail.com>
2024-12-19 09:49:05 +0530

    commit

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,9 +4,10 @@ import gificon from '../assets/new_removalicon.gif'
 
 function Navbar() {
   return (
-    <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
+    <div className='flex items-center justify-between mx-4 py-3 lg:mx-44 '>
        <img className='w-32 sm:w-44' src={assets.logo} alt='logo'/>
-       <button>
+       <button className='bg-zinc-800 text-white flex items-center gap-4 
+       px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full'>
         Get started <img src={assets.arrow_icon} alt='arrow'/>
        </button>
     </div>
