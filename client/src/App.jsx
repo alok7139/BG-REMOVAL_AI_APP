ec8946add6616aa7a22eb17e16ba547a4831dea6
alok7139 <alokgarg7139@gmail.com>
2024-12-19 09:18:17 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import Buycredit from './pages/Buycredit'
 
 function App() {
   return (
     <div className='bg-red-400'>
-      App
+       <Routes>
+         <Route path='/' element= {<Home/>}/>
+         <Route path='/result' element={<Result/>}/>
+         <Route path='/buy/credit' element={<Buycredit/>}/>
+       </Routes>
     </div>
   )
 }
