fd1fa30aea106e64b618eb54302a6b3c9dbab7e5
alok7139 <alokgarg7139@gmail.com>
2024-12-19 12:12:45 +0530

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Steps from '../Components/Steps'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
     </div>
   )
 }
