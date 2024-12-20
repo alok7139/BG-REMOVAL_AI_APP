c021c279da641201607736e47552f68a8ad790c5
alok7139 <alokgarg7139@gmail.com>
2024-12-20 13:10:04 +0530

    commit

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -4,9 +4,9 @@ import { assets } from '../assets/assets'
 function Footer() {
   return (
     <div className='flex items-center justify-between gap-4 px-4 lg:px-44 py-3'>
-        <img src={assets.logo} alt='logo'/>
-        <p>Copyright bg.removal | All right reserved.</p>
-        <div>
+        <img width={150} src={assets.logo} alt='logo'/>
+        <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright bg.removal | All right reserved.</p>
+        <div className='flex gap-1'>
             <img src={assets.facebook_icon} width={40} />
             <img width={40} src={assets.twitter_icon}/>
             <img width={40} src={assets.google_plus_icon}/>
